67bd2cf8f5156ed6710a3c5156b87d2db9d92e43
Arkadiy Lobuznov <arkadiyarkadiy95@gmail.com>
2024-05-05 21:37:17 +0700

    correted mistakes again in cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,5 @@ import readlineSync from 'readline-sync';
 const greeting = () => {
   const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
   console.log(`Hello, ${name}!`);
-  return;
 };
 export default greeting;
